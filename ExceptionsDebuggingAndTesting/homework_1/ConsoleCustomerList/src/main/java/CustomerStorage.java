37c120a9b04582ce07c40655b8d417fe3d68450d
Maxim Ryabchenko <99169411+Plemash@users.noreply.github.com>
2022-02-17 10:04:34 +0300

    ExceptionsDebugginAndTesting
    Starting to work with exceptions and testing

---
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -0,0 +1,47 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CustomerStorage {
+    private final Map<String, Customer> storage;
+
+    public CustomerStorage() {
+        storage = new HashMap<>();
+    }
+
+    public void addCustomer(String data) {
+        final int INDEX_NAME = 0;
+        final int INDEX_SURNAME = 1;
+        final int INDEX_EMAIL = 2;
+        final int INDEX_PHONE = 3;
+
+        String[] components = data.split("\\s+");
+        if (components.length != 4) {
+            throw new IllegalArgumentException("Invalid data entry format. Use the correct template: " +
+                    "add Василий Петров vasily.petrov@gmail.com +79215637722");
+        }
+        String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
+        if (!components[INDEX_EMAIL].matches(".+@.+(ru|com)") || !components[INDEX_PHONE].matches("(\\+\\d{11})$")) {
+            throw new IllegalArgumentException("Invalid data entry format. Use the correct template:" +
+                    "Email: hello@skillbox.ru Phone: +79991234567");
+        }
+        storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
+
+    }
+
+
+    public void listCustomers() {
+        storage.values().forEach(System.out::println);
+    }
+
+    public void removeCustomer(String name) {
+        storage.remove(name);
+    }
+
+    public Customer getCustomer(String name) {
+        return storage.get(name);
+    }
+
+    public int getCount() {
+        return storage.size();
+    }
+}
